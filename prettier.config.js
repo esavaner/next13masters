ff6294cf6f48ff38e51df50a662e0f8bee3f429f
esavaner <44378917+esavaner@users.noreply.github.com>
2023-09-21 02:37:51 +0200

    inital repo

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: './tailwind.config.ts',
+};
